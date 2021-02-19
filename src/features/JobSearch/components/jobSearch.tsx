d588020fed37cf73d372cba5247dd40588a1480b
Victor Anderssén <victor.anderssen@gmail.com>
2021-02-19 17:27:15 +0000

    Possible to search for only location

--- src/features/JobSearch/components/jobSearch.tsx
+++ src/features/JobSearch/components/jobSearch.tsx
@@ -71,7 +71,7 @@ export const JobSearch: React.FC<JobSearchProps> = () => {
             >
                 <div className="flex flex-row md:flex-col w-full md:w-1/4">
                     <input
-                        ref={register({ required: true, minLength: 2 })}
+                        ref={register({ required: false, minLength: 2 })}
                         autoComplete="off"
                         name="query"
                         className={`flex-1 appearance-none border border-transparent w-full py-2 px-4 bg-white text-gray-800 placeholder-gray-500 shadow-md rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-400 focus:border-transparent`}
